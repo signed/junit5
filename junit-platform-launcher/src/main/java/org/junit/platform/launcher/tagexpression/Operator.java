dff1fcb707bad1685b174a23bb6fc013d9c84b4d
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2017-12-07 00:32:28 +0100

    reduce duplication

--- junit-platform-launcher/src/main/java/org/junit/platform/launcher/tagexpression/Operator.java
+++ junit-platform-launcher/src/main/java/org/junit/platform/launcher/tagexpression/Operator.java
@@ -12,6 +12,9 @@ package org.junit.platform.launcher.tagexpression;
 
 import static org.junit.platform.launcher.tagexpression.Associativity.Left;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 class Operator {
 
 	static Operator nullaryOperator(String representation, int precedence) {
@@ -19,12 +22,39 @@ class Operator {
 	}
 
 	static Operator unaryOperator(String representation, int precedence, Associativity associativity,
-			ExpressionCreator expressionCreator) {
+			Function<Expression, Expression> unaryExpression) {
+		ExpressionCreator expressionCreator = (expressions, position) -> {
+			Position<Expression> rhs = expressions.pop();
+			if (position < rhs.position) {
+				Expression not = unaryExpression.apply(rhs.element);
+				expressions.push(new Position<>(position, not));
+				return ParseStatus.success();
+			}
+			return ParseStatus.Create(position, representation, "missing rhs operand");
+		};
 		return new Operator(representation, precedence, 1, associativity, expressionCreator);
 	}
 
 	static Operator binaryOperator(String representation, int precedence, Associativity associativity,
-			ExpressionCreator expressionCreator) {
+			BiFunction<Expression, Expression, Expression> binaryExpression) {
+		ExpressionCreator expressionCreator = (expressions, position) -> {
+			Position<Expression> rhs = expressions.pop();
+			Position<Expression> lhs = expressions.pop();
+			if (lhs.position < position && position < rhs.position) {
+				expressions.push(new Position<>(position, binaryExpression.apply(lhs.element, rhs.element)));
+				return ParseStatus.success();
+			}
+
+			if (position > rhs.position) {
+				return ParseStatus.Create(position, representation, "missing rhs operand");
+			}
+			if (position < lhs.position) {
+				return ParseStatus.missingOperatorBetween(lhs.position, lhs.element.toString(), rhs.position,
+					rhs.element.toString());
+			}
+			return ParseStatus.problemParsing(position, representation);
+		};
+
 		return new Operator(representation, precedence, 2, associativity, expressionCreator);
 	}
 
@@ -43,9 +73,9 @@ class Operator {
 		this.expressionCreator = expressionCreator;
 	}
 
-    boolean represents(String token) {
-        return representation.equals(token);
-    }
+	boolean represents(String token) {
+		return representation.equals(token);
+	}
 
 	String representation() {
 		return representation;
@@ -55,13 +85,13 @@ class Operator {
 		return this.precedence < operator.precedence;
 	}
 
-    boolean hasSamePrecedenceAs(Operator operator) {
-        return this.precedence == operator.precedence;
-    }
+	boolean hasSamePrecedenceAs(Operator operator) {
+		return this.precedence == operator.precedence;
+	}
 
-    boolean isLeftAssociative() {
-        return Left == associativity;
-    }
+	boolean isLeftAssociative() {
+		return Left == associativity;
+	}
 
 	ParseStatus createAndAddExpressionTo(Stack<Position<Expression>> expressions, int position) {
 		if (expressions.size() < arity) {
--- junit-platform-launcher/src/main/java/org/junit/platform/launcher/tagexpression/Operators.java
+++ junit-platform-launcher/src/main/java/org/junit/platform/launcher/tagexpression/Operators.java
@@ -14,59 +14,16 @@ import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toMap;
 import static org.junit.platform.launcher.tagexpression.Associativity.Left;
 import static org.junit.platform.launcher.tagexpression.Associativity.Right;
-import static org.junit.platform.launcher.tagexpression.Expressions.and;
 import static org.junit.platform.launcher.tagexpression.Expressions.not;
-import static org.junit.platform.launcher.tagexpression.Expressions.or;
 
 import java.util.Map;
 import java.util.stream.Stream;
 
 class Operators {
 
-	private static final Operator Not = Operator.unaryOperator("!", 3, Right, (expressions, position) -> {
-		Position<Expression> rhs = expressions.pop();
-		if (position < rhs.position) {
-			Expression not = not(rhs.element);
-			expressions.push(new Position<>(position, not));
-			return ParseStatus.success();
-		}
-		return ParseStatus.Create(position, "!", "missing rhs operand");
-	});
-
-	private static final Operator And = Operator.binaryOperator("&", 2, Left, (expressions, position) -> {
-		Position<Expression> rhs = expressions.pop();
-		Position<Expression> lhs = expressions.pop();
-		if (lhs.position < position && position < rhs.position) {
-			expressions.push(new Position<>(position, and(lhs.element, rhs.element)));
-			return ParseStatus.success();
-		}
-
-		if (position > rhs.position) {
-			return ParseStatus.Create(position, "&", "missing rhs operand");
-		}
-		if (position < lhs.position) {
-			return ParseStatus.missingOperatorBetween(lhs.position, lhs.element.toString(), rhs.position,
-				rhs.element.toString());
-		}
-		return ParseStatus.problemParsing(position, "&");
-	});
-
-	private static final Operator Or = Operator.binaryOperator("|", 1, Left, (expressions, position) -> {
-		Position<Expression> rhs = expressions.pop();
-		Position<Expression> lhs = expressions.pop();
-		if (lhs.position < position && position < rhs.position) {
-			expressions.push(new Position<>(position, or(lhs.element, rhs.element)));
-			return ParseStatus.success();
-		}
-		if (position > rhs.position) {
-			return ParseStatus.Create(position, "|", "missing rhs operand");
-		}
-		if (position < lhs.position) {
-			return ParseStatus.missingOperatorBetween(lhs.position, lhs.element.toString(), rhs.position,
-				rhs.element.toString());
-		}
-		return ParseStatus.problemParsing(position, "|");
-	});
+	private static final Operator Not = Operator.unaryOperator("!", 3, Right, Expressions::not);
+	private static final Operator And = Operator.binaryOperator("&", 2, Left, Expressions::and);
+	private static final Operator Or = Operator.binaryOperator("|", 1, Left, Expressions::or);
 
 	private final Map<String, Operator> representationToOperator = Stream.of(Not, And, Or).collect(
 		toMap(Operator::representation, identity()));
